d7fc5fb7a8e29117530070bbc48e99a95acd63ac
LeoDev9 <leonardoDevelopper924@gmail.com>
2023-10-27 22:05:16 +0100

    new

---
+++ src/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style =
+        {
+            {
+                backgroundColor: 'tomato', 
+                display:'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center',
+                color: 'white',
+                height: "100vh"
+            }
+        }>
+            <h2>
+                home
+            </h2>
+        </div>
+    )
+}
+
+export default Home
