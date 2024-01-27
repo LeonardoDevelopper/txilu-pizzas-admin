dd0268e84003825b67aa5b5053bfe65eb5b27d23
LeoDev9 <leonardoDevelopper924@gmail.com>
2024-01-27 10:00:33 +0100

    done

--- src/pages/logo/logo.jsx
+++ src/pages/logo/logo.jsx
@@ -5,7 +5,7 @@ import "./logo.css"
 const Logo = () => {
     return (
         <div className="container-logo" >
-            <Img path={'http://localhost:8080/uploads/txilu-pizzas-logo-no-background.png'} type={"img-logo-big"} />
+            <Img path={'http://192.168.130.241:8080/uploads/pizzas/6f61884e-6ae4-4989-bada-93762983e9b2.jpeg'} type={"img-logo-big"} />
         </div>
     )
 }
